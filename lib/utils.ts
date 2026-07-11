cb8592081fc9c03eb27f8bcb442a11d001eaf895
aniket866 <iamaniketkumarmaner@gmail.com>
2026-07-11 21:02:28 +0530

    build-fix
    Signed-off-by: aniket866 <iamaniketkumarmaner@gmail.com>

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,6 @@ type ClassValue = string | false | 0 | undefined | null | ClassValue[];
 export function cn(...inputs: ClassValue[]) {
   return inputs
     .flat()
-    .filter((x): x is string => typeof x === 'string' && x)
+    .filter((x): x is string => typeof x === 'string' && !!x)
     .join(' ');
 }
